0216c55ee14de96531ed66fd4a7d32032004396a
HenryToti <henryokoth3@gmail.com>
2024-05-14 13:16:03 +0300

    Initial Commit

---
+++ task-management/task_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_managementApp.swift
+//  task-management
+//
+//  Created by Billy Okoth on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct task_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
