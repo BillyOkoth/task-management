1245bbfd2c9f88a9509646a40708c415494c7e36
HenryToti <henryokoth3@gmail.com>
2024-05-15 16:41:23 +0300

    intergration of swift data persistence

---
+++ task-management/View/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  task-management
+//
+//  Created by Billy Okoth on 15/05/2024.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
